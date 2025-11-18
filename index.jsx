5ac822a80e867dc8b6efba014bb7a1b5732a45a1
Lalman <lalmanchaudhary2@gmail.com>
2025-11-18 17:23:24 +0530

    feat:adding nested routing

--- index.jsx
+++ index.jsx
@@ -1,33 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "/src/pages/Home";
+import About from "/src/pages/About";
+import Vans from "/src/pages/Vans";
+import VanDetail from "/src/pages/VanDetail";
+import Layout from "/src/components/Layout";
 
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "/src/pages/Home.jsx"
-import About from "/src/pages/About"
-import Vans from "/src/pages/Vans"
-import VanDetail from "/src/pages/VanDetail"
-
-import "./server"
+import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
