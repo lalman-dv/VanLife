d3511e0af2ca017fc8f17285774f31e347d7460c
Lalman <lalmanchaudhary2@gmail.com>
2025-11-22 22:02:06 +0530

    fix: used NavLinkProps.end

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const HostLayout = () => {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <>
       <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/reviews">Reviews</Link>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          end
+          to="/host"
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          to="/host/income"
+        >
+          Income
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          to="/host/reviews"
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
