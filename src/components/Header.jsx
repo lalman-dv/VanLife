90e0a90ef35afbc564607626f24b3b3dd2e4b995
Lalman <lalmanchaudhary2@gmail.com>
2025-11-22 21:54:51 +0530

    feat:active link with NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 const Header = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <header>
       <Link className="site-logo" to="/">
         #VanLife
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+          to="/host"
+        >
+          Host
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+          to="/vans"
+        >
+          Vans
+        </NavLink>
       </nav>
     </header>
   );
